fc8e8a014e839de6a2d536df1b22d58b8345e76b
Miguel de la Cruz <miguel@mcrx.me>
2016-06-07 18:18:23 +0200

    Add :aot :all

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://baoqu.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aot :all
   :main baoqu-core.core
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[lein-cucumber "1.0.2"]
