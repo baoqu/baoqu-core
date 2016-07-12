a12ceb23ffa33772c279d100f084f8c0a66a2f81
Miguel de la Cruz <miguel@mcrx.me>
2016-07-12 17:42:13 +0200

    Add core.async test to a SSE handler

--- src/baoqu_core/core.clj
+++ src/baoqu_core/core.clj
@@ -1,15 +1,30 @@
 (ns baoqu-core.core
   (:require [catacumba.core :as ct]
-            [catacumba.handlers.misc :as misc]))
+            [clojure.core.async :refer [go-loop chan mult tap close! <! >! >!!]]))
+
+(def c (chan 10))
+(def m (mult c))
 
 (defn example-handler
   [ctx]
+  (>!! c "WHATEVEEEEEEEEER")
   {:status 200
    :body "Hello BAOQU"})
 
+(defn sse-handler
+  {:handler-type :catacumba/sse}
+  [ctx out]
+  (let [local-c (chan)
+        _ (tap m local-c)]
+    (go-loop []
+      (when-let [msg (<! local-c)]
+        (if-not (>! out msg)
+          (close! local-c)
+          (recur))))))
+
 (def app
-  (ct/routes [[:any (misc/autoreloader)]
-              [:all "" #'example-handler]]))
+  (ct/routes [[:any "sse" #'sse-handler]
+              [:any "" example-handler]]))
 
 (defn -main
   [& args]
