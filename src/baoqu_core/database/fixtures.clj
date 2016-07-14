00f43c60c601b06e91ccf0e9ac8490732b741365
Miguel de la Cruz <miguel@mcrx.me>
2016-07-14 17:30:47 +0200

    Fixtures for 9th PIWEEK demo

--- src/baoqu_core/database/fixtures.clj
+++ src/baoqu_core/database/fixtures.clj
@@ -6,21 +6,23 @@
 
 (defn load-all
   []
-  (let [event (event-service/create "Decision maker 2000" 3 3)
+  (let [event (event-service/create "Ciclismo en la Guindalera, una discusión abierta" 3 3)
 
         ;; Users
-        user1 (user-service/create "test1")
-        user2 (user-service/create "test2")
-        user3 (user-service/create "test3")
-        user4 (user-service/create "test4")
-        user5 (user-service/create "test5")
-        user6 (user-service/create "test6")
-        user7 (user-service/create "test7")
-        user8 (user-service/create "test8")
-        user9 (user-service/create "test9")
-        user10 (user-service/create "test10")
+        user1 (user-service/create "Ramira")
+        user2 (user-service/create "Maria")
+        user3 (user-service/create "Yamilo")
+        user4 (user-service/create "Mario")
+        user5 (user-service/create "Pérez-Reverte")
+        user6 (user-service/create "Yisus")
+        user7 (user-service/create "Magikarp")
 
-        idea-name "Win the Xth PIWEEK"]
+        idea-name1 "En la décima PIWEEK lo arreglamos todo"
+        idea-name2 "Nos hacen falta más carriles bici"
+        idea-name3 "Las bicis son para hippies, ¡cómo se nota que no trabajais!"
+        idea-name4 "Cómo conectar con el centro: bulevar en Juan Bravo"
+        idea-name5 "Sin presupuestos es difícil hacer nada"
+        idea-name6 "Hay que arreglar los baches, son un peligro"]
 
     ;; Add users to event
     (println "++ Adding users")
@@ -31,24 +33,39 @@
     (event-manager-service/add-user-to-event user5 event)
     (event-manager-service/add-user-to-event user6 event)
     (event-manager-service/add-user-to-event user7 event)
-    (event-manager-service/add-user-to-event user8 event)
-    (event-manager-service/add-user-to-event user9 event)
-    (event-manager-service/add-user-to-event user10 event)
 
     ;; Vote ideas
     (println "++ Voting ideas")
-    (event-manager-service/upvote user1 idea-name)
-    (def idea (idea-service/get-by-name idea-name))
-    (event-manager-service/upvote user2 idea-name)
-    (event-manager-service/upvote user3 idea-name)
-    (event-manager-service/upvote user4 idea-name)
-    (event-manager-service/upvote user5 idea-name)
-    (event-manager-service/upvote user6 idea-name)
-    (event-manager-service/upvote user7 idea-name)
-    (event-manager-service/upvote user8 idea-name)
-    (event-manager-service/upvote user9 idea-name)
+    (event-manager-service/upvote user1 idea-name1)
+    (event-manager-service/upvote user2 idea-name1)
+    (event-manager-service/upvote user3 idea-name1)
+    (event-manager-service/upvote user4 idea-name1)
+    (event-manager-service/upvote user5 idea-name1)
+    (event-manager-service/upvote user6 idea-name1)
+    (event-manager-service/upvote user7 idea-name1)
+
+    (event-manager-service/upvote user1 idea-name2)
+    (event-manager-service/upvote user2 idea-name2)
+
+    (event-manager-service/upvote user3 idea-name3)
+    (event-manager-service/upvote user4 idea-name3)
+
+    (event-manager-service/upvote user4 idea-name4)
+    (event-manager-service/upvote user5 idea-name4)
+
+    (event-manager-service/upvote user6 idea-name5)
+    (event-manager-service/upvote user7 idea-name5)
+
+    (event-manager-service/upvote user7 idea-name6)
+
+    ;(def idea1 (idea-service/get-by-name idea-name1))
+    ;(def idea2 (idea-service/get-by-name idea-name2))
+    ;(def idea3 (idea-service/get-by-name idea-name3))
+    ;(def idea4 (idea-service/get-by-name idea-name4))
+    ;(def idea5 (idea-service/get-by-name idea-name5))
+    (def idea6 (idea-service/get-by-name idea-name6))
 
     ;; Downvote ideas
-    (event-manager-service/downvote user1 idea)
+    (event-manager-service/downvote user7 idea6)
 
     ))
