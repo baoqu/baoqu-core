8f33c32561bf97823b8916c914d36a4ed6089de6
Miguel de la Cruz <miguel@mcrx.me>
2016-07-14 17:43:19 +0200

    Remove withCredentials and allow "*" in CORS filter

--- src/baoqu_core/handlers/middleware.clj
+++ src/baoqu_core/handlers/middleware.clj
@@ -3,11 +3,10 @@
 
 (defn cors-handler
   [ctx]
-  (ct/set-headers! ctx {:Access-Control-Allow-Origin "http://localhost:3449"})
+  (ct/set-headers! ctx {:Access-Control-Allow-Origin "*"})
   (ct/set-headers! ctx {:Access-Control-Expose-Headers "*"})
   (ct/set-headers! ctx {:Access-Control-Allow-Headers "X-Requested-With, Content-Type"})
   (ct/set-headers! ctx {:Access-Control-Allow-Methods "GET, POST, PUT, DELETE, OPTIONS"})
-  (ct/set-headers! ctx {:Access-Control-Allow-Credentials "true"})
   (if (= (:method ctx) :options)
     {:status 200
      :body ""}
