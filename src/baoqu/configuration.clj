f42028052803dfe65e9173b0019b44a9f2cc9e65
Miguel de la Cruz <miguel@mcrx.me>
2016-11-09 19:16:22 +0100

    Change server port

--- src/baoqu/configuration.clj
+++ src/baoqu/configuration.clj
@@ -9,5 +9,5 @@
 
 (def config
   {:db-path (env-or "BC_DB_PATH" default-database-path)
-   :server-port (Integer/parseInt (env-or "BC_SERVER_PORT" "3000"))
-   :server-url (env-or "BC_SERVER_URL" "http://localhost:3000")})
+   :server-port (Integer/parseInt (env-or "BC_SERVER_PORT" "5050"))
+   :server-url (env-or "BC_SERVER_URL" "http://localhost:5050")})
