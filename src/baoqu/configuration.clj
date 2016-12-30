3d49ee01160777422d3389885f33924a988ed231
Miguel de la Cruz <miguel@mcrx.me>
2016-12-30 14:45:53 +0100

    Add environ to manage config

--- src/baoqu/configuration.clj
+++ src/baoqu/configuration.clj
@@ -1,13 +1,10 @@
-(ns baoqu.configuration)
-
-(defn env-or
-  [env default]
-  (or (System/getenv env) default))
+(ns baoqu.configuration
+  (:require [environ.core :refer [env]]))
 
 (def default-database-path
   (str (System/getProperty "user.dir") "/baoqu-core-db.sqlite"))
 
 (def config
-  {:db-path (env-or "BC_DB_PATH" default-database-path)
-   :server-port (Integer/parseInt (env-or "BC_SERVER_PORT" "5050"))
-   :server-url (env-or "BC_SERVER_URL" "http://localhost:5050")})
+  {:db-path (env :bc-db-path default-database-path)
+   :server-port (Integer/parseInt (env :bc-server-port "5050"))
+   :server-url (env :bc-server-url (str "http://localhost:" (env :bc-server-port "5050")))})
