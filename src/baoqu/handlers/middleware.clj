52e7cc973c08ee2e66fe8dbd895fc08e67efa759
Miguel de la Cruz <miguel@mcrx.me>
2017-11-12 12:27:49 +0000

    Add authorization to the CORS header

--- src/baoqu/handlers/middleware.clj
+++ src/baoqu/handlers/middleware.clj
@@ -7,7 +7,7 @@
   [ctx]
   (ct/set-headers! ctx {:Access-Control-Allow-Origin "*"})
   (ct/set-headers! ctx {:Access-Control-Expose-Headers "*"})
-  (ct/set-headers! ctx {:Access-Control-Allow-Headers "X-Requested-With, Content-Type"})
+  (ct/set-headers! ctx {:Access-Control-Allow-Headers "X-Requested-With, Content-Type, Authorization"})
   (ct/set-headers! ctx {:Access-Control-Allow-Methods "GET, POST, PUT, DELETE, OPTIONS"})
   (if (= (:method ctx) :options)
     {:status 200
